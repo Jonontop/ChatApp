ff9e8ea695c22363e6c140320414c0fe30a99db9
NinjaCof <jon.pecar@gmail.com>
2024-02-09 18:36:18 +0100

    Create register.js

---
+++ client/src/components/register/register.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Register() {
+    return( 
+        <div>
+            <h1>Register</h1>
+            <p>This is page for registration</p>
+        </div>
+    );
+}
+
+export default Register;
